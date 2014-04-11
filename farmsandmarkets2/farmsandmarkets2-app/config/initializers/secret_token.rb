9861e880e3737c71334daaecad2bad9ea20bce81
Cindy Brown <ceeblet@gmail.com>
2014-04-11 19:56:52 +0000

    Initial commit

---
+++ farmsandmarkets2/farmsandmarkets2-app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Farmsandmarkets2App::Application.config.secret_key_base = 'e2a017ffb00a1041e23160048b503ddebd5a5bc8cf24a75f07cf7b879a2a9329afd968320aed188fea44a3226659d576c7555c2125027f2ff485e530174cdaab'
---
+++ farmsandmarkets2/farmsandmarkets2-app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Farmsandmarkets2App::Application.config.session_store :cookie_store, key: '_farmsandmarkets2-app_session'
